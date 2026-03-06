1f3f4502a739e64496df0a2d312aecc503db980f
Rendel05 <th851627@gmail.com>
2026-03-06 02:44:49 -0600

    fix(api):Bug fixes for deployment in Vercel

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+
 import suppliersRoutes from './src/routes/suppliersRoutes.js'
 import categoriesRoutes from './src/routes/categoriesRoutes.js'
 import authRoutes from './src/routes/authRoutes.js'
@@ -9,22 +10,18 @@ import pagesRoutes from './src/routes/editablePagesRoutes.js'
 import customersRoutes from './src/routes/customersRoutes.js'
 import cashierRoutes from './src/routes/cashierRoutes.js'
 
-
-
-
 const app = express()
 
 app.use(express.json())
+
 app.use('/api', suppliersRoutes)
 app.use('/api', categoriesRoutes)
-app.use('/api', authRoutes) 
+app.use('/api', authRoutes)
 app.use('/api', userRoutes)
 app.use('/api', productsRoutes)
 app.use('/api', offersRoutes)
 app.use('/api', pagesRoutes)
-app.use('/api',customersRoutes)
-app.use('/api',cashierRoutes)
+app.use('/api', customersRoutes)
+app.use('/api', cashierRoutes)
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000')
-})
+export default app
